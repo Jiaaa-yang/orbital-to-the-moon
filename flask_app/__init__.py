058f6db1a9f5d93cf05bda5ba9f536074fe690e4
ovidharshini <ovidharshini@gmail.com>
2021-05-28 19:25:28 +0800

    Add symbol verification
    Included alert for symbols not in given list.

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -4,6 +4,9 @@ from . import home
 def create_app():
     app = Flask(__name__)
 
+    #Throwaway secret key; use environment variables or config files locally
+    app.secret_key = 'W84Ngvr4mfhyb20ga2lja3MgYXNz'
+
     app.register_blueprint(home.bp)
 
     return app
--- flask_app/home.py
+++ flask_app/home.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, redirect, render_template, request, url_for
+    Blueprint, redirect, render_template, request, url_for, flash
 )
 
 bp = Blueprint('main', __name__)
@@ -9,11 +9,34 @@ def home():
     if request.method == 'POST':
         ticker = request.form['ticker']
         print(ticker)
-        return redirect(url_for('main.result'))
+        return result(ticker)
 
     return render_template('index.html')
 
 
 @bp.route('/results')
-def result():
-    return render_template('results.html')
+def result(search_str):
+	results = []
+	TOP_TRADED_SYMBOLS = {'MARA', 'M', 'LGHL', 'SU', 'PALI', 'PSFE', 'ABR', 'QS', 'JD', 'ATOS', 'VZ', 'BSX', 'VIAC', 'TSLA', 'BB', 'NOK',
+                     'INTC', 'AAPL', 'IQ', 'QCOM', 'AAL', 'EDU', 'OPGN', 'VTNR', 'RKT', 'VRM', 'FCX', 'TWTR', 'SOS', 'MS', 'EWZ', 'NKLA',
+                     'F', 'BAC', 'NRZ', 'XLE', 'MDRR', 'TECK', 'BP', 'ING', 'AZN', 'IDEX', 'LI', 'MVIS', 'RBLX', 'AJG', 'GOLD', 'CSPR',
+                     'TELL', 'FSR', 'JMIA', 'ACB', 'KO', 'NCLH', 'FB', 'CVX', 'SIRI', 'MGM', 'VIPS', 'T', 'TME', 'PLUG', 'RMED', 'APPS',
+                     'WISH', 'SNAP', 'CPE', 'DKNG', 'DIS', 'RMO', 'DDD', 'IWM', 'UEC', 'WKHS', 'FUBO', 'CCIV', 'MT', 'TIGR', 'GE', 'ZNGA',
+                     'HRTX', 'MU', 'SONO', 'NEM', 'JNJ', 'MSFT', 'EBON', 'BBVA', 'HBAN', 'NKE', 'FHN', 'BRFS', 'C', 'AMWL', 'UWMC', 'AMAT', 
+                     'BILI', 'HL', 'OPEN', 'CPNG', 'NIO', 'ARRY', 'AHT', 'WFC', 'BMBL', 'FAMI', 'SQQQ', 'CTRM', 'SOXL', 'CRIS', 'ARKK', 'NERV',
+                     'GLW', 'LUV', 'SPY', 'SAN', 'IBM', 'ITUB', 'SPCE', 'GNW', 'KEY', 'CCL', 'CSCO', 'FSLY', 'TSM', 'PTON', 'AUY', 'CS', 
+                     'HPQ', 'LMND', 'FSM', 'GSK', 'LTHM', 'CNHI', 'AGNC', 'SID', 'TAL', 'AEG', 'LU', 'PYPL', 'EXK', 'UAL', 'PAAS', 'MRK', 
+                     'CAN', 'XOM', 'UBER', 'ET', 'AR', 'WY', 'OCGN', 'HAL', 'NVDA', 'X', 'SNDL', 'BNGO', 'BNTC', 'IHT', 'GGB', 'RIG', 'BIDU', 
+                     'SRNG', 'ON', 'FTEK', 'OXY', 'NLY', 'COIN', 'BBD', 'COP', 'TQQQ', 'PINS', 'DAL', 'SLB', 'CLF', 'SI', 'PLTR', 'HAE', 'KMI', 
+                     'RIOT', 'FCEL', 'EYES', 'MRO', 'XPEV', 'BABA', 'BOX', 'EEM', 'AMC', 'BA', 'PBR', 'PSTG', 'UUUU', 'BIL', 'KOS', 'TLRY', 'FUTU', 
+                     'PG', 'CMCSA', 'LHDX', 'AUTL', 'AMD', 'VALE', 'XLF', 'QQQ', 'JPM', 'SKLZ', 'GEVO', 'GME', 'SQ', 'UMC', 'KGC', 'NNDM', 'PFE',
+                     'MRNA', 'GM', 'SNOW'}
+	
+	if search_str in TOP_TRADED_SYMBOLS:
+		return render_template('results.html') 
+
+	if search_str != "":
+		flash('No matching company listing found')
+		return redirect('/')
+
+	return render_template('index.html')
