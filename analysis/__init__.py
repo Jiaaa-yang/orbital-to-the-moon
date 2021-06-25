7af04cde7cf6e34097cb9e361b8eba82865f552b
Jia Yang <lyejy99@gmail.com>
2021-06-25 17:10:57 +0800

    Update app.py for deployment to heroku
    Made changes to app.py so that the app can be
    run by gunicorn. Also added a Procfile and .slugignore
    for heroku deployment. Also changed spacy model from
    en_core_web_lg to en_core_web_sm to fit into heroku
    slug size.

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,4 +1,4 @@
 import spacy
 
 # Load text processing pipeline model from spacy
-nlp = spacy.load('en_core_web_lg')
+nlp = spacy.load('en_core_web_sm')
--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 """Runner module to start flask application
 
 """
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 from flask_app import create_app
 
 
 app = create_app()
-app.run()
+# app.run()
