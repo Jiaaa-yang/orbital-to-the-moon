ed8e762d21479dc6357419661d866b685ecf4a1e
Jia Yang <lyejy99@gmail.com>
2021-07-25 19:32:57 +0800

    Make session of favourite stocks permanent

--- app.py
+++ app.py
@@ -4,7 +4,13 @@
 # from dotenv import load_dotenv
 # load_dotenv()
 from flask_app import create_app
+from flask import session
 
 
 app = create_app()
+
+@app.before_first_request
+def make_session_permanent():
+    session.permanent = True
+
 # app.run()
