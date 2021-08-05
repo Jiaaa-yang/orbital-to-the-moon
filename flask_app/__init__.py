c1428d6caf1e8a0f7ccbf9050855b86eb27ea7ea
Jia Yang <lyejy99@gmail.com>
2021-08-05 16:33:26 +0800

    Set up static folder to be at root path of application
    Set static url path to be root path of app and also moved
    favicons and apple touch icons to static folder

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -4,7 +4,7 @@ from .views import home, analysis
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static', static_url_path='')
 
     app.register_blueprint(home.bp)
     app.register_blueprint(analysis.bp)
